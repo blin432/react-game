7b97169c48fc57b1d9a7b9bcc5a560893150c368
Benjamin Lin <blin432@gmail.com>
2019-08-19 16:47:58 -0400

    products

--- src/components/Product.js
+++ src/components/Product.js
@@ -58,6 +58,37 @@ export default class Product extends Component {
   }
 }
 
+export default class Navbar extends Component{
+    render(){
+        return(
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            {/*
+            https://www.iconfinder.com/icons/1243689/call_phone_icon
+            Creative Commons (Attribution 3.0 Unported);
+            https://www.iconfinder.com/Makoto_msk 
+            */}
+            <Link to="/">
+                <img src={logo} alt="store" className="navbar-brand" />
+            </Link>
+            <ul className="navbar-nav align-items-center">
+                <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                        Products
+                    </Link>
+                </li>
+            </ul>
+            <Link to="/cart" className="ml-auto">
+            <ButtonContainer>
+                <span className="mx">
+                    <i className="fas fa-cart-plus"></i>
+                </span>
+            </ButtonContainer>
+            </Link>
+            </NavWrapper>
+        )
+    }
+}
+
 Product.propTypes = {
     product: PropTypes.shape({
         id:PropTypes.number,
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -31,3 +31,34 @@ export default class ProductList extends Component{
         )
     }
 }
+
+export default class Navbar extends Component{
+    render(){
+        return(
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            {/*
+            https://www.iconfinder.com/icons/1243689/call_phone_icon
+            Creative Commons (Attribution 3.0 Unported);
+            https://www.iconfinder.com/Makoto_msk 
+            */}
+            <Link to="/">
+                <img src={logo} alt="store" className="navbar-brand" />
+            </Link>
+            <ul className="navbar-nav align-items-center">
+                <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                        Products
+                    </Link>
+                </li>
+            </ul>
+            <Link to="/cart" className="ml-auto">
+            <ButtonContainer>
+                <span className="mx">
+                    <i className="fas fa-cart-plus"></i>
+                </span>
+            </ButtonContainer>
+            </Link>
+            </NavWrapper>
+        )
+    }
+}
