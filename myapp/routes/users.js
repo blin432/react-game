26075f148ab892fed7d718230ab11568d55361a3
Benjamin Lin <blin432@gmail.com>
2019-09-22 20:13:11 -0400

    routes

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/register', function(req, res, next){
+  let hashedPassword = bcrypt.hashsyn(password,10);
+  return db.one('INSERT INTO users (username, password_hash) VALUES ($1, $2) returning id', [userdname, hasedPassword])
+})
+.then(() =>{
+  res.send('success')
+})
+.catch(function(error){
+  console.log("error", error);
+  res.status(400).send(error)
+});
+
+
+
+  
+
 module.exports = router;
